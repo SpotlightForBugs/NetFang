6076e386b308f738d1d94e2cadea34c32fee5ab0
Johannes Häusler <mail@spotlightforbugs.eu>
2025-03-14 19:05:09 +0100

    feat: add psutil dependency and improve network event data handling
    Signed-off-by: Johannes Häusler <mail@spotlightforbugs.eu>

--- netfang/setup/udev_receiver.py
+++ netfang/setup/udev_receiver.py
@@ -1,17 +1,18 @@
+import sys
+
 import requests
 
 
 def send_data(event_type, interface_name):
-    url = 'http://localhost:80/api/network_event'
-    data = {'event_type': event_type}
-    response = requests.post(url, data=data)
+    url = 'http://127.0.0.1:80/api/network_event'
+    data = {'event_type': event_type, 'interface_name': interface_name}
+    response = requests.post(url, data=data, timeout=5)
     print(response.text)
 
 
 if __name__ == '__main__':
+    print(sys.argv)
     # this can be called with python udev_receiver.py cable_inserted|connected|disconnected eth0|eth1|eth2|eth3|eth4
-    import sys
-
     event_type = sys.argv[1]
     interface_name = sys.argv[2]
     send_data(event_type, interface_name)
