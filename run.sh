c166da6b58183784131e1805f3b81d543188277b
Johannes Häusler <mail@spotlightforbugs.eu>
2025-03-15 00:59:39 +0100

    fix: improve formatting and structure of argument parsing in run.sh
    Signed-off-by: Johannes Häusler <mail@spotlightforbugs.eu>

--- run.sh
+++ run.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
-    update_repo=false
-    run_hidden=false
+update_repo=false
+run_hidden=false
 
-    while [[ $# -gt 0 ]]; do
-      case $1 in
-        -h)
-          echo "\Usage: ./run.sh [OPTIONS]"
-          echo "\  -h         Display help"
-          echo "\  -u         Update the repository"
-          echo "\  --hidden   Run in the background"
-          exit 0
-          ;;
-        -u)
-          update_repo=true
-          shift
-          ;;
-        --hidden)
-          run_hidden=true
-          shift
-          ;;
-        *)
-          shift
-          ;;
-      esac
-    done
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h)
+      echo "Usage: ./run.sh [OPTIONS]"
+      echo "  -h         Display help"
+      echo "  -u         Update the repository"
+      echo "  --hidden   Run in the background"
+      exit 0
+      ;;
+    -u)
+      update_repo=true
+      shift
+      ;;
+    --hidden)
+      run_hidden=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
 
-    if [ "$update_repo" = true ]; then
-      git pull
-    fi
+if [ "$update_repo" = true ]; then
+  git pull
+fi
 
-    chmod +x `netfang/setup/arp_helper.py`
-    sudo python `netfang/setup/setup_manager.py` &&
+chmod +x netfang/setup/arp_helper.py
+chmod +x netfang/setup/setup_manager.py
 
-    if [ "$run_hidden" = true ]; then
-      nohup sudo python -m netfang.main &
-    else
-      sudo python -m netfang.main
-    fi
+sudo python netfang/setup/setup_manager.py &&
 
-    sudo python `netfang/setup/setup_manager.py` stop
+if [ "$run_hidden" = true ]; then
+  sudo python -m netfang.main &
+else
+   python -m netfang.main
+fi
+
+sudo python netfang/setup/setup_manager.py stop
